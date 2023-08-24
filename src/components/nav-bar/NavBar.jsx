f952b826502191a541adc15050567119f29ee661
wizardy <wizardhassan99@gmail.com>
2023-08-24 19:48:04 +0100

    commit

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,11 +1,5 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './navBar.scss'
-
-import AssistantOutlinedIcon from '@mui/icons-material/AssistantOutlined';
-import TerminalOutlinedIcon from '@mui/icons-material/TerminalOutlined';
-import WorkspacePremiumOutlinedIcon from '@mui/icons-material/WorkspacePremiumOutlined';
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 
 function NavBar() {
   return (
