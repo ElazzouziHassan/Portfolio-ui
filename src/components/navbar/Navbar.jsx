b907d159786d797972f6e2f45de144d87cc4d724
wizardy <wizardhassan99@gmail.com>
2023-08-24 20:37:41 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,23 @@ import './navbar.scss'
 function Navbar() {
   return (
     <div className="nav-bar">
-      <h1>Nav Bar</h1>
+      <div className="left">
+        <div className="logo">
+          <h2>Elazzouzi H<span>.</span></h2>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Works</li>
+            <li>Resume</li>
+          </ul>
+        </div>
+      </div>
+      <div className="right">
+        <input type="button" value="Let's Talk" />
+      </div>
     </div>
   )
 }
