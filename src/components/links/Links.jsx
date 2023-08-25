39ddfc5b2d1732401f442ad303295b23d5490be0
wizardy <wizardhassan99@gmail.com>
2023-08-25 20:25:56 +0100

    commit

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './links.scss'
+import {
+  LinkedInIcon,
+  GitHubIcon,
+  FacebookIcon,
+  InstagramIcon
+} from '@mui/icons-material/LinkedIn';
 
 function Links() {
   return (
     <div className="links">
-      <a href="#" target="_blank">LinkedIn</a>
-      <a href="#" target="_blank">Github</a>
-      <a href="#" target="_blank">Facebook</a>
-      <a href="#" target="_blank">Instagrm</a>
+      <a href="#" target="_blank"><LinkedInIcon className="icon"/></a>
+      <a href="#" target="_blank"><GitHubIcon className="icon"/></a>
+      <a href="#" target="_blank"><FacebookIcon className="icon"/></a>
+      <a href="#" target="_blank"><InstagramIcon className="icon"/></a>
     </div>
   )
 }
