e14d2e991230caf010d78d8b03bc89eff5544773
wizardy <wizardhassan99@gmail.com>
2023-08-25 12:28:35 +0100

    commit

---
+++ src/components/topbanner/Topbanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './topbanner.scss'
+
+function Topbanner() {
+  return (
+    <div className="top-banner">
+      <h1>Top Banner</h1>
+    </div>
+    
+  )
+}
+
+export default Topbanner
