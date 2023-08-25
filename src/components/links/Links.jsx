2c95216a8f309c6bd634c111049d358e8cf09a92
wizardy <wizardhassan99@gmail.com>
2023-08-25 20:19:47 +0100

    commit

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -4,7 +4,10 @@ import './links.scss'
 function Links() {
   return (
     <div className="links">
-      
+      <a href="#" target="_blank">LinkedIn</a>
+      <a href="#" target="_blank">Github</a>
+      <a href="#" target="_blank">Facebook</a>
+      <a href="#" target="_blank">Instagrm</a>
     </div>
   )
 }
