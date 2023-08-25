9ad49ae2b483a0bb66672a48b0c1ea21141d1f35
wizardy <wizardhassan99@gmail.com>
2023-08-25 12:33:43 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar"
+import Topbanner from "./components/topbanner/Topbanner"
 
 
 function App() {
 
   return (
     <div className="App">
+      <Topbanner/>
       <Navbar />
       <hr />
     </div>
