e9d4ad06817568657898ba6b69066757f3bbf246
wizardy <wizardhassan99@gmail.com>
2023-08-25 12:33:49 +0100

    commit

--- src/components/topbanner/Topbanner.jsx
+++ src/components/topbanner/Topbanner.jsx
@@ -4,7 +4,16 @@ import './topbanner.scss'
 function Topbanner() {
   return (
     <div className="top-banner">
-      <h1>Top Banner</h1>
+      <div className="left">
+        <a href="mailto:ezhassan.info@gmail.com">Send me mail</a>
+        <a href="tel:+212 648540458">Call me</a>
+      </div>
+      <div className="right">
+        <a href="#" target="_blank" rel="Github">Github</a>
+        <a href="#" target="_blank" rel="LinkedIn">LinkedIn</a>
+        <a href="#" target="_blank" rel="Facebook">Facebook</a>
+        <a href="#" target="_blank" rel="Instagram">Instagram</a>
+      </div>
     </div>
     
   )
