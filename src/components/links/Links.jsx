40e8a7107b3428e9ddd8fdf1d0fd95cbf3e6d936
wizardy <wizardhassan99@gmail.com>
2023-08-25 20:32:29 +0100

    commit

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -8,10 +8,10 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 function Links() {
   return (
     <div className="links">
-      <a href="#" target="_blank"><LinkedInIcon className="icon"/></a>
-      <a href="#" target="_blank"><GitHubIcon className="icon"/></a>
-      <a href="#" target="_blank"><FacebookIcon className="icon"/></a>
-      <a href="#" target="_blank"><InstagramIcon className="icon"/></a>
+      <a href="https://www.linkedin.com/in/elazzouzihassan/" target="_blank"><LinkedInIcon className="icon"/></a>
+      <a href="https://github.com/ElazzouziHassan" target="_blank"><GitHubIcon className="icon"/></a>
+      <a href="https://web.facebook.com/itsmewizardy/" target="_blank"><FacebookIcon className="icon"/></a>
+      <a href="https://www.instagram.com/_wizardy__/" target="_blank"><InstagramIcon className="icon"/></a>
     </div>
   )
 }
