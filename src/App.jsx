f9693f6e46952926a557a5084f23cc6bd2deec7f
wizardy <wizardhassan99@gmail.com>
2023-08-26 12:09:16 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+
 import MenuBlinder from "./components/decorateurs/MenuBlinder"
-import Links from "./components/links/Links"
 import Navbar from "./components/navbar/Navbar"
 
 
 function App() {
 
+  const { darkMode } = useContext(DarkModeContext);
+
+  const Layout = () => {
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <Navbar />
+        <hr />
+        <MenuBlinder />
+        {/* <Outlet/> */}
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <hr />
-      <MenuBlinder />
+      
       
     </div>
   )
