1c3285c7f7236eb3c3d914a9dbbd3f8a2a0ffff0
wizardy <wizardhassan99@gmail.com>
2023-08-27 12:53:38 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './style.scss'
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Services from "./pages/services/Services"
+import About from "./pages/about/About"
+import Works from "./pages/works/Works"
+import Resume from "./pages/resume/Resume"
 
 
 function App() {
@@ -14,15 +19,21 @@ function App() {
     return (
       <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
-        <hr />
-        {/* <Outlet/> */}
+        <Outlet/>
       </div>
     )
   }
 
   const router = createBrowserRouter([
     { path: '/', element: <Layout />,
-      children: []
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/about', element: <About /> },
+        { path: '/services', element: <Services/> },
+        { path: '/works', element: <Works /> },
+        { path: '/resume', element: <Resume /> },
+        
+      ]
     },
   ]);
 
