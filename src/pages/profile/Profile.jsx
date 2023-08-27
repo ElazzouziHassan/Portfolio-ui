f4e061b06525c6b3ac6dd91dbbe5286ab07248c8
wizardy <wizardhassan99@gmail.com>
2023-08-27 20:50:32 +0100

    commit

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './profile.scss'
+
+function Profile() {
+  return (
+    <div className="profile">
+      <h1>profile page</h1>
+    </div>
+  )
+}
+
+export default Profile
