854b2e404f8ea6e9efba95090656369b3117b61f
wizardy <wizardhassan99@gmail.com>
2023-08-30 19:56:17 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,8 @@
 import './navbar.scss'
-import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
-import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 import { DarkModeContext } from '../../context/darkModeContext';
 
 
-import HouseboatOutlinedIcon from '@mui/icons-material/HouseboatOutlined';
 
 function Navbar() {
 
@@ -15,58 +10,11 @@ function Navbar() {
 
   return (
     <div className="nav-bar">
-      <div className="left">
-        <div className="logo">
-          <Link to="/profile" style={{ textDecoration:'none' }}>
-            <h2>ELAZZOUZI <span>.HA</span></h2>
-          </Link>
-        </div>
-        <div className="menu">
-          <ul>
-            <li>
-              <Link to="/">
-                <span>HOME</span>
-                <ArrowCircleRightOutlinedIcon className="icon" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">
-                <span>ABOUT</span>
-                <ArrowCircleRightOutlinedIcon className="icon" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/services">
-                <span>SERVICES</span>
-                <ArrowCircleRightOutlinedIcon className="icon" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/works">
-                <span>WORKS</span>
-                <ArrowCircleRightOutlinedIcon className="icon" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/resume">
-                <span>RESUME</span>
-                <ArrowCircleRightOutlinedIcon className="icon" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="right">
-        <button onClick={toggle}>
+      <button onClick={toggle}>
         {
-            darkMode
-              ? 
-                <span><WbSunnyOutlinedIcon  className='icon'/></span>
-              :
-                <span><DarkModeOutlinedIcon className='icon'/></span>
+          darkMode ? "light" : "Dark"
         }
-        </button>
-      </div>
+      </button>
     </div>
   )
 }
