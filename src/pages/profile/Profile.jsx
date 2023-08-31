92c2e0967b40c24ea6a0866c15d889cc8e214b41
wizardy <wizardhassan99@gmail.com>
2023-08-31 19:58:21 +0100

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,28 +4,7 @@ import './profile.scss'
 function Profile() {
   return (
     <div className="profile">
-      <div className="top">
-        <div className="user-card">
-          <div className="user">
-            <img src="#" alt="user" />
-            <span>Jhone Doe</span>
-            <span>badge</span>
-            <h2>Title</h2>
-          </div>
-          <div className="action">
-            <button>Let's Talk</button>
-          </div>
-        </div>
-        <div className="intro">
-          <span>Intro</span>
-          <hr />
-          <div className="info">
-          </div>
-        </div>
-      </div>
-      <div className="posts">
-        <h1>posts</h1>
-      </div>
+      
     </div>
   )
 }
