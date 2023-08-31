6c4f45097a105c044fde0a6bb79f4f36aa8a6afb
wizardy <wizardhassan99@gmail.com>
2023-08-31 20:08:51 +0100

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './footer.scss'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="left"></div>
-      <div className="center"></div>
-      <div className="right"></div>
+      <div className="left">
+        <Link to='/profile'><h2>ELAZZOUZI <span>.HA</span></h2></Link>
+      </div>
+      <div className="center">
+        <span>Copyright <a href="#">Wizardy</a> &copy;2023</span>
+      </div>
+      <div className="right">
+        
+      </div>
     </div>
   )
 }
