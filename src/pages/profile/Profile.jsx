0238e82dda87b206553ff3c3cbf974ba32153a8e
wizardy <wizardhassan99@gmail.com>
2023-08-31 20:00:38 +0100

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './profile.scss'
+import Footer from '../../components/footer/Footer'
 
 function Profile() {
   return (
     <div className="profile">
       
+
+      <Footer/>
     </div>
   )
 }
