121115e1e8e3a3b8bcf0ddce21dc4fef769f1e0b
wizardy <wizardhassan99@gmail.com>
2023-08-31 18:45:39 +0100

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,12 +12,14 @@ function Navbar() {
     <div className="nav-bar">
       <div className="left"></div>
       <div className="menu"></div>
-      <div className="right"></div>
-      <button onClick={toggle}>
-        {
-          darkMode ? "light" : "Dark"
-        }
-      </button>
+      <div className="right">
+        <button onClick={toggle}>
+          {
+            darkMode ? "light" : "Dark"
+          }
+        </button>
+      </div>
+      
     </div>
   )
 }
