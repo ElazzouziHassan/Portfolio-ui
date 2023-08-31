0defa28215437b8a127d9ab2fdd29d7b0d3d5a47
wizardy <wizardhassan99@gmail.com>
2023-08-31 19:05:57 +0100

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,7 +4,19 @@ import './profile.scss'
 function Profile() {
   return (
     <div className="profile">
-      <h1>profile page</h1>
+      <div className="top">
+        <div className="user-card">
+
+        </div>
+        <div className="intro">
+          
+        </div>
+      </div>
+      <div className="posts">
+        <h1>posts</h1>
+      </div>
+
+
     </div>
   )
 }
