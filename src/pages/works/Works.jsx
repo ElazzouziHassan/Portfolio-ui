f7bf5e6ee555a2bbcc4e2445e8e351ec2613277f
wizardy <wizardhassan99@gmail.com>
2023-09-01 19:13:12 +0100

    commit

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './works.scss'
-import Leftbar from '../../components/_works/_leftbar/Leftbar'
-import Main from '../../components/_works/_main/Main'
 
 function Works() {
   return (
     <div className="works">
-      <Leftbar />
-      <Main/>
+      <h1>works</h1>
     </div>
   )
 }
