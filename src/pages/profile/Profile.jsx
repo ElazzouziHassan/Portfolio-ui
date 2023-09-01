d5fd0004b41f4afbdf61364bdf44e386501a0baa
wizardy <wizardhassan99@gmail.com>
2023-09-01 18:33:49 +0100

    commit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import './profile.scss'
-import Footer from '../../components/footer/Footer'
 
 function Profile() {
   return (
     <div className="profile">
-      <div className="main">
-        <div className="left">
-          left
-        </div>
-        <div className="right">
-          <div className="top">
-            right-top
-          </div>
-          <div className="bottom">
-            right-bottom
-          </div>
-        </div>
-      </div>
-      <Footer/>
+      <h1>Profile</h1>
     </div>
   )
 }
