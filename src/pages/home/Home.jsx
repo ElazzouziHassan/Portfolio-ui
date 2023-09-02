1d2e66520c4425c4b359d0cb4370645313d9df32
wizardy <wizardhassan99@gmail.com>
2023-09-02 18:46:49 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,48 @@ function Home() {
   return (
     <div className="home">
       <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
     </div>
   )
 }
