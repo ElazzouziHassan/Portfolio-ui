2ff84aa2ab514f03c3ebc256419ba08b572742dd
wizardy <wizardhassan99@gmail.com>
2023-09-04 21:27:36 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,31 +9,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 function Home() {
   return (
     <div className="home">
-      <div className="top">
-        <div className="user">
-          <img src={User} alt="user" />
-          <div className="social-network">
-            <a href="#"><GitHubIcon className='icon github'/></a>
-            <a href="#"><LinkedInIcon className='icon linkedin'/></a>
-            <a href="#"><InstagramIcon className='icon instgram'/></a>
-            <a href="#"><FacebookIcon className='icon facebook' /></a>
-          </div>
-          <div className="arab-name">
-            <span style={{ fontFamily: "Amiri Quran" }}>العزوزي حسن</span>
-          </div>  
-        </div>
-        <div className="banner">
-          <h1>EL AZZOUZI HASSAN</h1>
-        </div>
-      </div> 
-      <div className="bottom">
-        <div className="left">
-          <h1>left side</h1>
-        </div>
-        <div className="right">
-          <h1>right side</h1>
-        </div>
-      </div>    
+      <h1>home page</h1>  
     </div>
   )
 }
