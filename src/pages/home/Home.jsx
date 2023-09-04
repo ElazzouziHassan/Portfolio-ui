234a0762452ad522f0ac1ea6980348df367066bd
wizardy <wizardhassan99@gmail.com>
2023-09-04 21:28:47 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,5 @@
 import './home.scss'
 import React from 'react'
-import User from '../../assets/user.jpg'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Home() {
   return (
