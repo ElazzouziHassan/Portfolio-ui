1490ec77b3807865c8bfcf8277896e9d5b9fff97
wizardy <wizardhassan99@gmail.com>
2023-09-05 16:46:07 +0100

    commit

--- src/components/homeComponents/hero/HeroSection.jsx
+++ src/components/homeComponents/hero/HeroSection.jsx
@@ -4,7 +4,11 @@ import './HeroSection.scss'
 function HeroSection() {
   return (
     <div className="hero-section">
-      <h1>hero section</h1>
+      <div className="left">
+        <h1>EL AZZOUZI HASSAN</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita numquam rem quis quam iusto, maxime amet quo. Quis temporibus sed nobis rerum accusamus saepe praesentium vel qui, repellendus ut odio.</p>
+        <input type="button" value="Let's Talk" />
+      </div>
     </div>
   )
 }
