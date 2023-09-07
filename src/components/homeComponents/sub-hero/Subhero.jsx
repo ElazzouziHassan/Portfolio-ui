a222adc0487ba9317eebfcb3b67aa3a76fab649d
wizardy <wizardhassan99@gmail.com>
2023-09-07 22:28:40 +0100

    commit

---
+++ src/components/homeComponents/sub-hero/Subhero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './subhero.scss'
+
+function Subhero() {
+  return (
+    <div className="sub-hero">
+      <div className="cards">
+        <div className="card">
+          <img src="#" alt="logo" />
+          <h3>Frontend Developement</h3>
+        </div>
+        <div className="card">
+          <img src="#" alt="logo" />
+          <h3>Backend Developement</h3>
+        </div>
+        <div className="card">
+          <img src="#" alt="logo" />
+          <h3>UI/ UX Design</h3>
+        </div>
+        <div className="card">
+          <img src="#" alt="logo" />
+          <h3>Database management</h3>
+        </div>
+        <div className="card">
+          <img src="#" alt="logo" />
+          <h3>API Developement</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Subhero
