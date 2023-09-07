3c41463103abe8e4a2b98025eae5e414fbe5117a
wizardy <wizardhassan99@gmail.com>
2023-09-07 21:23:53 +0100

    commit

--- src/components/homeComponents/hero/HeroSection.jsx
+++ src/components/homeComponents/hero/HeroSection.jsx
@@ -10,32 +10,31 @@ function HeroSection() {
         <h2>Full Stack Web Developer & UI/UX Designer</h2>
         <h3>Where Innovation Meets Imagination: Your Web Solutions Expert.</h3>
         <p> 
-          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
+          <CheckBoxOutlinedIcon className='list-icon'/>
           <span className='list-text'>
             Bringing Together Code and Creativity to Craft Exceptional Web Experiences.
           </span><br />
         </p>
-
         <p>
-        <span><CheckBoxOutlinedIcon className='list-icon'/></span>
+        <CheckBoxOutlinedIcon className='list-icon'/>
           <span className='list-text'>
             Transforming Ideas into Digital Realities with a Fusion of Skills.
           </span><br />
 </p>
         <p>
-        <span><CheckBoxOutlinedIcon className='list-icon'/></span>
+        <CheckBoxOutlinedIcon className='list-icon'/>
           <span className='list-text'>
             Empowering Brands with Exceptional Full Stack Web and Design Solutions. 
           </span><br />
           </p>
         <p>
-        <span><CheckBoxOutlinedIcon className='list-icon'/></span>
+        <CheckBoxOutlinedIcon className='list-icon'/>
           <span className='list-text'>
             Your Partner for Next-Level Digital Experiences: Code & Design Combined.
           </span><br />
           </p>
         <p>
-          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
+          <CheckBoxOutlinedIcon className='list-icon'/>
           <span className='list-text'>
             Designing Tomorrow, Building Today: Your Full Stack & UI/UX Expert.
           </span><br />
