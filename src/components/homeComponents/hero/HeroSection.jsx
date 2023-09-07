07b8b154658459809e84779dccc93e2dff953014
wizardy <wizardhassan99@gmail.com>
2023-09-07 20:59:30 +0100

    commit

--- src/components/homeComponents/hero/HeroSection.jsx
+++ src/components/homeComponents/hero/HeroSection.jsx
@@ -8,11 +8,27 @@ function HeroSection() {
       <div className="left">
         <h1>EL AZZOUZI HASSAN</h1>
         <p>
-          <span>-</span><span></span><br />
-          <span>-</span><span></span><br />
-          <span>-</span><span></span><br />
-          <span>-</span><span></span><br />
-          <span>-</span><span></span><br />
+          <h3>Where Innovation Meets Imagination: Your Web Solutions Expert.</h3>
+          <span>-</span>
+          <span>
+            Bringing Together Code and Creativity to Craft Exceptional Web Experiences.
+          </span><br />
+          <span>-</span>
+          <span>
+            Transforming Ideas into Digital Realities with a Fusion of Skills.
+          </span><br />
+          <span>-</span>
+          <span>
+            Empowering Brands with Exceptional Full Stack Web and Design Solutions. 
+          </span><br />
+          <span>-</span>
+          <span>
+            Your Partner for Next-Level Digital Experiences: Code & Design Combined.
+          </span><br />
+          <span>-</span>
+          <span>
+            Designing Tomorrow, Building Today: Your Full Stack & UI/UX Expert.
+          </span><br />
         </p>
         <input type="button" value="Let's Talk" />
       </div>
