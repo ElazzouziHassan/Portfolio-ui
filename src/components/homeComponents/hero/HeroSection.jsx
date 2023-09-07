f56e4d479fb02e66d2178c39739487054708c42a
wizardy <wizardhassan99@gmail.com>
2023-09-07 21:06:56 +0100

    commit

--- src/components/homeComponents/hero/HeroSection.jsx
+++ src/components/homeComponents/hero/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HeroSection.scss'
+import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
 
 function HeroSection() {
 
@@ -9,23 +10,23 @@ function HeroSection() {
         <h1>EL AZZOUZI HASSAN</h1>
         <p>
           <h3>Where Innovation Meets Imagination: Your Web Solutions Expert.</h3>
-          <span>-</span>
+          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
           <span>
             Bringing Together Code and Creativity to Craft Exceptional Web Experiences.
           </span><br />
-          <span>-</span>
+          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
           <span>
             Transforming Ideas into Digital Realities with a Fusion of Skills.
           </span><br />
-          <span>-</span>
+          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
           <span>
             Empowering Brands with Exceptional Full Stack Web and Design Solutions. 
           </span><br />
-          <span>-</span>
+          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
           <span>
             Your Partner for Next-Level Digital Experiences: Code & Design Combined.
           </span><br />
-          <span>-</span>
+          <span><CheckBoxOutlinedIcon className='list-icon'/></span>
           <span>
             Designing Tomorrow, Building Today: Your Full Stack & UI/UX Expert.
           </span><br />
