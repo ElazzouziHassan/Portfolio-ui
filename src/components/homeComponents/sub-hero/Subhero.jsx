8ee7e09ec4c55e45022f2be9e84359f237f6ba89
wizardy <wizardhassan99@gmail.com>
2023-09-08 18:55:08 +0100

    commit

--- src/components/homeComponents/sub-hero/Subhero.jsx
+++ src/components/homeComponents/sub-hero/Subhero.jsx
@@ -7,33 +7,24 @@ import {
   UilServer,
   UilDesktopAlt 
 } from '@iconscout/react-unicons'
-import API from '../../../assets/home/api.png'
-import Backend from '../../../assets/home/backend.png'
-import Frontend from '../../../assets/home/frontend.png'
-import Database from '../../../assets/home/database.png'
-import UiUx from '../../../assets/home/uiux.png'
 
 function Subhero() {
   return (
     <div className="sub-hero">
       <div className="cards">
         <div className="card">
-          {/* <img src={Frontend} alt="logo" /> */}
           <UilReact className='ui-icon'/>
           <h3>Frontend Developement</h3>
         </div>
         <div className="card">
-          {/* <img src={Backend} alt="logo" /> */}
           <UilServer className='ui-icon'/>
           <h3>Backend Developement</h3>
         </div>
         <div className="card">
-          {/* <img src={UiUx} alt="logo" /> */}
           <UilDesktopAlt className='ui-icon' />
           <h3>UI/ UX Design</h3>
         </div>
         <div className="card">
-          {/* <img src={Database} alt="logo" /> */}
           <UilDatabase className='ui-icon'/>
           <h3>Database management</h3>
         </div>
