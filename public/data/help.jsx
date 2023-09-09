ba798c7d2b800d06fc9f02bc39842271252e498c
wizardy <wizardhassan99@gmail.com>
2023-09-09 19:43:06 +0100

    commit

---
+++ public/data/help.jsx
@@ -0,0 +1,22 @@
+<div className="social-media">
+          <a href='#'>
+            <GitHubIcon className='s-icon github' />
+            <span>GitHub</span>
+          </a>
+          <a href='#'>
+            <LinkedInIcon className='s-icon linkedIn' />
+            <span>LinkedIn</span>
+          </a>
+          <a href='#'>
+            <FacebookIcon className='s-icon facebook' />
+            <span>Facebook</span>
+          </a>
+          <a href='#'>
+            <TwitterIcon className='s-icon twitter' />
+            <span>Twitter</span>
+          </a>
+          <a href='#'>
+            <InstagramIcon className='s-icon instagram' />
+            <span>Instagram</span>
+          </a>
+        </div>
