755bcce986892e12ca2d11692e7be499acc0aa1f
wizardy <wizardhassan99@gmail.com>
2023-09-09 19:59:02 +0100

    commit

--- src/components/homeComponents/sub-hero/Subhero.jsx
+++ src/components/homeComponents/sub-hero/Subhero.jsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import './subhero.scss'
-import {
-  UilReact,
-  UilCloudComputing,
-  UilDatabase,
-  UilServer,
-  UilDesktopAlt 
-} from '@iconscout/react-unicons'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Subhero() {
   return (
     <div className="sub-hero">
-      <div className="cards">
-        <div className="card">
-          <UilReact className='ui-icon'/>
-          <h3>Frontend Developement</h3>
-        </div>
-        <div className="card">
-          <UilServer className='ui-icon'/>
-          <h3>Backend Developement</h3>
-        </div>
-        <div className="card">
-          <UilDesktopAlt className='ui-icon' />
-          <h3>UI/ UX Design</h3>
-        </div>
-        <div className="card">
-          <UilDatabase className='ui-icon'/>
-          <h3>Database management</h3>
-        </div>
-        <div className="card">
-          <UilCloudComputing className='ui-icon'/>
-          <h3>API Developement</h3>
-        </div>
+      <div className="hr">
+        <hr />
+      </div>
+      <div className="social-media">
+      <a href='#'>
+            <GitHubIcon className='s-icon github' />
+            <span>GitHub</span>
+          </a>
+          <a href='#'>
+            <LinkedInIcon className='s-icon linkedIn' />
+            <span>LinkedIn</span>
+          </a>
+          <a href='#'>
+            <FacebookIcon className='s-icon facebook' />
+            <span>Facebook</span>
+          </a>
+          <a href='#'>
+            <TwitterIcon className='s-icon twitter' />
+            <span>Twitter</span>
+          </a>
+          <a href='#'>
+            <InstagramIcon className='s-icon instagram' />
+            <span>Instagram</span>
+          </a>
       </div>
     </div>
   )
