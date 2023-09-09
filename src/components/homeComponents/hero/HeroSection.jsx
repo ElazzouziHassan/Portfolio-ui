62e8b09b09d1da4e8ab2cb0027216722684f7b86
wizardy <wizardhassan99@gmail.com>
2023-09-09 19:43:17 +0100

    commit

--- src/components/homeComponents/hero/HeroSection.jsx
+++ src/components/homeComponents/hero/HeroSection.jsx
@@ -50,28 +50,7 @@ function HeroSection() {
           <br />
         </p>
         <input type="button" value="Let's Talk" />
-        <div className="social-media">
-          <a href='#'>
-            <GitHubIcon className='s-icon github' />
-            <span>GitHub</span>
-          </a>
-          <a href='#'>
-            <LinkedInIcon className='s-icon linkedIn' />
-            <span>LinkedIn</span>
-          </a>
-          <a href='#'>
-            <FacebookIcon className='s-icon facebook' />
-            <span>Facebook</span>
-          </a>
-          <a href='#'>
-            <TwitterIcon className='s-icon twitter' />
-            <span>Twitter</span>
-          </a>
-          <a href='#'>
-            <InstagramIcon className='s-icon instagram' />
-            <span>Instagram</span>
-          </a>
-        </div>
+        
       </div>
       <div className="right">
         <img src="https://present.readthedocs.io/en/latest/_images/welcome-to-coding.gif" alt="hero image" />
